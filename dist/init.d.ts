238dd88c12b5e66d04c677948d33c94d5d78c050
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-03-13 19:43:16 +0300

    init

--- dist/init.d.ts
+++ dist/init.d.ts
@@ -1,1 +1,1 @@
-export declare function init(root: string, appName: string, verbose: boolean, originalDirectory: string, template: string): void;
+export declare function init(appPath: string, appName: string, verbose: boolean, originalDirectory: string, template: string): void;
